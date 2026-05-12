b71c38ab74008e1d8d13f58d44495aefbb8b0ecc
Sudiin <shresthasudin44@gmail.com>
2026-05-12 19:34:04 +0545

    learnt about list

--- lecturethree.py
+++ lecturethree.py
@@ -14,5 +14,25 @@ print(student)
 str = "Hellow"
 print(str[0])
 
-top = [85, 95, 76, 63, 48]
+top = [85, 95, 76, 63, 48] 
 print(top[1:-1])
+
+list = [ "banana", "apple", "grape", "orange"]
+# list.append(4)
+print(list)
+list.sort()
+print(list)
+list.sort(reverse=True)
+print(list)
+
+list.reverse()
+print(list)
+
+list.insert(2, 'litchi')
+print(list)
+
+list = [1, 3, 5, 9]
+list.remove(3)
+print(list)
+list.pop(2)
+print(list)
